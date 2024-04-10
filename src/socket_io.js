99093a6a9a5be24361bd63425282679ce54a854b
Reza <rezaaaa.z.1991@gmail.com>
2024-04-10 10:38:02 +0330

    A room added to the socket.io

--- src/socket_io.js
+++ src/socket_io.js
@@ -11,6 +11,17 @@ function initializeSocket(server) {
   io.on("connection", (socket) => {
     console.log("A user connected");
 
+    // Joining a room
+    const room = "room100";
+      socket.join(room);
+
+    // Listening for messages from a specific room
+    socket.on("sendMessage", (data) => {
+      const { room, message } = data;
+      io.to(room).emit("message", message);
+      console.log(`Message received in room ${room}: ${message}`);
+    });
+
     socket.on("disconnect", () => {
       console.log("User disconnected");
     });
