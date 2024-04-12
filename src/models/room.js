ef77da0a077b119d0fda00c856f373cecfc3c50e
Reza <rezaaaa.z.1991@gmail.com>
2024-04-12 14:25:32 +0330

    added member field to Room model

--- src/models/room.js
+++ src/models/room.js
@@ -6,6 +6,12 @@ const roomSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
   messages: {
     type: [String],
     default: [],
