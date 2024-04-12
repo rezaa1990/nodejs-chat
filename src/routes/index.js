9a75e6d4a9a660598d81faeeef8b89b33102fc70
Reza <rezaaaa.z.1991@gmail.com>
2024-04-12 14:26:27 +0330

    make room operation added to the app

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const login = require("./login");
 const register = require("./register");
 const getAllUsers = require("./getallusers");
+const makeRoom = require("./makeroom");
 
 router.post("/register", register);
 
 router.post("/login", login);
 
 router.get("/getallusers", getAllUsers);
+
+router.post("/makeroom", makeRoom);
 
 module.exports = router;
