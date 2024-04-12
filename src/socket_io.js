52210c53deb7761779e2c612c25a9e056df85bf0
Reza <rezaaaa.z.1991@gmail.com>
2024-04-12 17:06:48 +0330

    socket.io file updated to save messages to the respective room

--- src/socket_io.js
+++ src/socket_io.js
@@ -15,26 +15,23 @@ function initializeSocket(server) {
     socket.on("joinRoom", (room) => {
       console.log("data:", room);
       socket.join(room);
-      // Initialize message array for each room if not already exist
-      // if (!roomsMessages[room]) {
-      //   roomsMessages[room] = [];
-      // }
       console.log(`User joined room: ${room}`);
     });
 
     const Room = require("./models/room");
 
     socket.on("sendMessage", async (data) => {
-      const { room: receivedRoom, message } = data;
+      const { room: receivedRoom, message, loginedUser, selectedUser } = data;
       console.log("room, message", receivedRoom, message);
       try {
-        let room = await Room.findOne({ name: receivedRoom });
+        let room = await Room.findOne({ _id: receivedRoom });
         if (!room) {
-          await Room.create({ name: receivedRoom });
+          room = await Room.create({ _id: receivedRoom });
+          await room.save();
           console.log(`Room "${receivedRoom}" created.`);
         }
         // Add message to the respective room's messages array
-        await room.messages.push(message);
+        room.messages.push(message);
         await room.save();
         io.to(receivedRoom).emit("message", { room: receivedRoom, message });
         console.log(`Message received in room ${receivedRoom}: ${message}`);
@@ -42,7 +39,6 @@ function initializeSocket(server) {
         console.error("Error occurred while handling sendMessage:", error);
       }
     });
-
     socket.on("disconnect", () => {
       console.log("User disconnected");
     });
