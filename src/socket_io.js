3e0699504df3f5c1e0365acec791bed4d5581a46
Reza <rezaaaa.z.1991@gmail.com>
2024-04-12 10:20:50 +0330

    some changes to add a client to one room an sending messages to this room

--- src/socket_io.js
+++ src/socket_io.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const Room = require("./../src/models/room");
 
 function initializeSocket(server) {
   const io = new Server(server, {
@@ -8,28 +9,38 @@ function initializeSocket(server) {
     },
   });
 
-  // Dictionary to store messages for each room
-  const roomsMessages = {};
-
   io.on("connection", (socket) => {
     console.log("A user connected");
 
     socket.on("joinRoom", (room) => {
-      console.log('data:',room);
+      console.log("data:", room);
       socket.join(room);
       // Initialize message array for each room if not already exist
-      if (!roomsMessages[room]) {
-        roomsMessages[room] = [];
-      }
+      // if (!roomsMessages[room]) {
+      //   roomsMessages[room] = [];
+      // }
       console.log(`User joined room: ${room}`);
     });
 
-    socket.on("sendMessage", (data) => {
-      const { room, message } = data;
-      // Add message to the respective room's messages array
-      roomsMessages[room].push(message);
-      io.to(room).emit("message", { room, message }); // Send room along with message
-      console.log(`Message received in room ${room}: ${message}`);
+    const Room = require("./models/room");
+
+    socket.on("sendMessage", async (data) => {
+      const { room: receivedRoom, message } = data;
+      console.log("room, message", receivedRoom, message);
+      try {
+        let room = await Room.findOne({ name: receivedRoom });
+        if (!room) {
+          await Room.create({ name: receivedRoom });
+          console.log(`Room "${receivedRoom}" created.`);
+        }
+        // Add message to the respective room's messages array
+        await room.messages.push(message);
+        await room.save();
+        io.to(receivedRoom).emit("message", { room: receivedRoom, message });
+        console.log(`Message received in room ${receivedRoom}: ${message}`);
+      } catch (error) {
+        console.error("Error occurred while handling sendMessage:", error);
+      }
     });
 
     socket.on("disconnect", () => {
